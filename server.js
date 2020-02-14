55200885ca18da700771e989dfc7f15ea9443795
Fernando Henrique <liquuid@gmail.com>
2020-02-13 21:14:43 -0300

    CRUD

--- server.js
+++ server.js
@@ -2,18 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const app = express();
-
+app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/nodeapi',{ useUnifiedTopology: true , useNewUrlParser: true });
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+app.use('/api', require('./src/routes'));
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build Native  Apps',
-        url: 'http://github.com/facebook/react-native'
-    });
-    return res.send("Hello");
-});
 app.listen(3001);
